586ce599d5f64d26fc0d55a52a2b59cb9087895f
kovarnyirus <kovarnyi@gmail.com>
2019-12-27 18:59:03 +0300

    pust text add desctop

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,40 @@
 'use strict';
 $(document).ready(function () {
-  $('.gallery__list').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    arrows: false
-    // variableWidth: true
-  });
+//   $('.gallery__list').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     centerMode: true,
+//     arrows: false
+//     // variableWidth: true
+//   });
 
   $('.price__list').slick({
     dots: true,
-    infinite: false,
-    slidesToShow: 1,
-    centerMode: true,
+    infinite: true,
+    slidesToShow: 3,
     arrows: false,
-    variableWidth: true
+    responsive: [
+      {
+        breakpoint: 999999,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true,
+          dots: true,
+        }
+      }
+    ]
   });
 
   $('.reviews__list').slick({
@@ -62,11 +80,11 @@ $(document).ready(function () {
 
   $(".program__accordion-toggle").click(function () {
     $(this).closest('.program__item').toggleClass("open");
-  })
+  });
 
   $(".main-nav__toggle").click(function () {
     $(this).closest('.main-nav').toggleClass("main-nav--open");
-  })
+  });
 
   $("body").on('click', '[href*="#"]', function (e) {
     var fixed_offset = -2;
