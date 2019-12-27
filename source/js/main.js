0c6ac1e7f339d1acd19a767589f4dc25c161b53f
kovarnyirus <kovarnyi@gmail.com>
2019-12-27 03:46:00 +0300

    add gallery

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-$( document ).ready(function() {
+$(document).ready(function () {
   $('.gallery__list').slick({
     dots: true,
     infinite: true,
@@ -9,6 +9,7 @@ $( document ).ready(function() {
     arrows: false
     // variableWidth: true
   });
+
   $('.price__list').slick({
     dots: true,
     infinite: false,
@@ -16,6 +17,47 @@ $( document ).ready(function() {
     centerMode: true,
     arrows: false,
     variableWidth: true
+  });
+
+  $('.reviews__list').slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 1,
+    arrows: false,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 999999,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        }
+      },
+      {
+        breakpoint: 1478,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 
   $(".program__accordion-toggle").click(function () {
@@ -26,9 +68,9 @@ $( document ).ready(function() {
     $(this).closest('.main-nav').toggleClass("main-nav--open");
   })
 
-  $("body").on('click', '[href*="#"]', function(e){
+  $("body").on('click', '[href*="#"]', function (e) {
     var fixed_offset = -2;
-    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    $('html,body').stop().animate({scrollTop: $(this.hash).offset().top - fixed_offset}, 1000);
     e.preventDefault();
   });
 });
